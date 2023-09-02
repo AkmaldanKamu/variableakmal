d873b747c57a089592749aab52047e11b1eaf188
Akmalsaban <117637830+AkmaldanKamu@users.noreply.github.com>
2023-09-03 06:48:22 +0700

    Add files via upload

---
+++ variable1.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+void main(){
+  stdout.write("Nama: ");
+  var nama =  stdin.readLineSync();
+
+  stdout.write("kelas: ");
+  var kelas =  stdin.readLineSync();
+
+  stdout.write("alamat: ");
+  var alamat =  stdin.readLineSync();
+
+  stdout.write("Tempat Tanggal Lahir: ");
+  var ttg =  stdin.readLineSync();
+
+print("Nama: $nama");
+print("nama: $kelas");
+print("nama: $alamat");
+print("nama: $ttg");
+
+}
